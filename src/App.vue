12a6a560847db7de6824c757a7f1583c20921b63
liuxinxin1205 <1521171265@qq.com>
2018-05-25 12:50:35 +0800

    全局组件

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <comp></comp>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Comp from '@/components/comp/Comp'
+export default {
+  name: 'App',
+  components:{
+    Comp
+  }
+}
+</script>
+
+<style>
+#app {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+</style>
